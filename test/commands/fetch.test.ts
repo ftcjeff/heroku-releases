5a5b06e962b5a6f9ffc8cdb9ad002ad8885f1807
Jeffrey J. Walls <ftcjeff@gmail.com>
2018-07-06 12:19:24 -0600

    Test run

--- test/commands/fetch.test.ts
+++ test/commands/fetch.test.ts
@@ -2,11 +2,35 @@ import {expect, test} from '@oclif/test'
 
 describe('releases:fetch', () => {
   test
+  .nock('https://api.heroku.com', api => api
+    .get('/apps/jeff-test/releases')
+    .reply(200, [
+      {
+        created_at: '2018-01-22T15:24:24Z',
+        id: '28e520c5-1ba8-480c-be6f-29496a68943d',
+        user: {
+          email: 'ftcjeff@gmail.com'
+        },
+        version: 1
+      },
+      {
+        created_at: '2018-01-22T15:24:25Z',
+        id: '19c11dcf-b84a-4bb9-bca9-9034157c12d9',
+        user: {
+          email: 'ftcjeff@gmail.com'
+        },
+        version: 2
+      }
+    ]
+  ))
   .stdout()
-  .command(['releases:fetch', '-a', 'pure-fjord-62694'])
-  .it('runs releases:fetch with a known app', ctx => {
-    expect(ctx.stdout).to.contain('6517f983-2f93-48d1-87cb-bb16ccef2d9a')
-    expect(ctx.stdout).to.contain('28e520c5-1ba8-480c-be6f-29496a68943d')
+  .command(['releases:fetch', '-a', 'jeff-test'])
+  .it('runs releases:fetch with a mock app', ctx => {
+    expect(ctx.stdout).to.be.equal(
+      `Release info for jeff-test
+2 - 2018-01-22T15:24:25Z - 19c11dcf-b84a-4bb9-bca9-9034157c12d9 - ftcjeff@gmail.com
+1 - 2018-01-22T15:24:24Z - 28e520c5-1ba8-480c-be6f-29496a68943d - ftcjeff@gmail.com
+`)
   })
 
   test
